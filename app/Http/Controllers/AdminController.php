0674a9a7273bece63d4223ccc58e562256111055
WILZZZZZ <Willysianipar009@gmail.com>
2025-11-11 15:14:57 +0700

    Update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index()
-    {
-        // Menampilkan halaman admin utama
-        return view('admin.admin'); // pastikan file ini ada di resources/views/admin/admin.blade.php
-    }
+{
+    // Menampilkan halaman dashboard admin
+    return view('admin.dashboard');
+}
 }
